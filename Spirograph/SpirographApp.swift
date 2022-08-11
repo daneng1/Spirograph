450aee5e063244b6d70819912aa9c4ab6fccc786
daneng1 <dan.engel@carerev.com>
2022-08-11 23:09:13 +0000

    initial build

---
+++ Spirograph/SpirographApp.swift
@@ -0,0 +1,17 @@
+    //
+    //  spirograph.swift
+    //  playarea
+    //
+    //  Created by Dan Engel on 7/14/22.
+    //
+
+import SwiftUI
+
+@main
+struct SpirographApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
